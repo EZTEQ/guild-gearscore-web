11f80a83239d82c6d11fd9f448b6c6a65194a530
Mark Hähnel <hello@markhaehnel.de>
2016-10-16 20:05:07 +0200

    Removed image load error handling

--- src/components/GuildMember.vue
+++ src/components/GuildMember.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="item">
-        <img class="ui rounded mini image" :src="avatar" @error="onAvatarError">
+        <img class="ui rounded mini image" :src="character.avatar">
         <div class="content">
             <span class="header">{{name}}</span>
             {{character.averageItemLevelEquipped}}
@@ -20,19 +20,10 @@ export default {
         character() {
             return this.$store.state.guild.members.filter(x => (x.name === this.name), this)[0];
         },
-        avatar() {
-            return this.avatarError ? '' : this.character.avatar;
-        },
     },
 
     mounted() {
         this.$store.dispatch('updateMember', { realm: this.character.realm, name: this.name });
-    },
-
-    methods: {
-        onAvatarError() {
-            this.avatarError = true;
-        },
     },
 };
 </script>
